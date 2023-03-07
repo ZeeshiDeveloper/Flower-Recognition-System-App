83abc387d4efd92c712e353878f2678bc98575bf
Zeeshan Ali <zee03044163645@gmail.com>
2023-03-07 20:58:20 +0500

    create  name api to send name by fetching from front end HomePage component in react-native, upload successfully image by gallary and camera to backend fast-api Predict Image file.

--- fast-api/predict_image.py
+++ fast-api/predict_image.py
@@ -1,4 +1,6 @@
 # from click import File
+import base64
+from typing import Optional
 import numpy as np
 import tensorflow as tf
 # load the saved model
@@ -30,34 +32,50 @@ async def root():
 async def root():
     return {"message": "Second Message"}
 
+from pydantic import BaseModel
 
+
+# Image comes from frond End (react-native>SCreens>HomePage)
+class ImageData(BaseModel):
+    my_image: bytes = File(...)
 @app.post("/upload")
-async def create_upload_file(file: bytes = File(...)):
-    # return{"filename" : file.filename}
-    print("jhfkasdfjsdfsghfgj")
-    image = Image.open(io.BytesIO(file))
+async def create_upload_file(payload: ImageData):
+    imgdata = base64.b64decode(payload.my_image)
+    with open("new_image.jpg", "wb") as f:
+        f.write(imgdata)
+    print(payload.my_image)
+
+    image = Image.open(io.BytesIO(imgdata))
     image.show()
-    print("dfsdfsdfsdfsfsdsfsdf")
     return {"Upload Status": "Completed"}
 
+# to Print the Name 
+class NameData(BaseModel):
+    my_name: str
 
-imag_path= './Prediction/dandelion.jpg'
-
-test_image = tf.keras.preprocessing.image.load_img(imag_path,target_size=(64,64))
-test_image = tf.keras.preprocessing.image.img_to_array(test_image)
-test_image = np.expand_dims(test_image,axis=0)
-result = model.predict(test_image)
+@app.post("/name")
+async def create_upload_file(payload: NameData):
+    print(payload.my_name)
+    return {"my_name": payload}
 
 
-print(result)
+# imag_path= './Prediction/dandelion.jpg'
 
-if result[0][0]==1:
-    print('Daisy')
-elif result[0][1]==1:
-    print('Dandelion')
-elif result[0][2]==1:
-    print('Rose')
-elif result[0][3]==1:
-    print('SunFlower')
-elif result[0][4]==1:
-    print("Tulip")
+# test_image = tf.keras.preprocessing.image.load_img(imag_path,target_size=(64,64))
+# test_image = tf.keras.preprocessing.image.img_to_array(test_image)
+# test_image = np.expand_dims(test_image,axis=0)
+# result = model.predict(test_image)
+
+
+# print(result)
+
+# if result[0][0]==1:
+#     print('Daisy')
+# elif result[0][1]==1:
+#     print('Dandelion')
+# elif result[0][2]==1:
+#     print('Rose')
+# elif result[0][3]==1:
+#     print('SunFlower')
+# elif result[0][4]==1:
+#     print("Tulip")
